11ba4d238bb6ef270a8ef2d432f6bd0eb5f25fb6
Ammon Casey <acasey@panda-group.com>
2019-05-31 09:05:44 -1000

    [config] Moved base_uri to defaults

--- config/config.php
+++ config/config.php
@@ -1,16 +1,23 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
-
-    'base_uri' => env('SABRE_BASE_URL', 'https://api-crt.cert.havail.sabre.com/'),
-
+    /*
+    |--------------------------------------------------------------------------
+    | Auth Credentials
+    |--------------------------------------------------------------------------
+    |
+    */
     'client_id' => env('SABRE_CLIENT_ID'),
     'client_secret'  => env('SABRE_CLIENT_SECRET'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | API defaults
+    |--------------------------------------------------------------------------
+    |
+    */
     'defaults' => [
+        'base_uri' => env('SABRE_BASE_URL', 'https://api.havail.sabre.com/'),
         'verify' => env('SABRE_API_VERIFY', true),
     ],
 ];
